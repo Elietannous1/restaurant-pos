8d019d30f4e8eb109403a560281fe3d34c9b4adc
Elietannous1 <tannous.elie.me@gmail.com>
2025-02-03 14:05:37 +0200

    Adds a registar function to the user

--- restaurant-pos/src/main/java/com/example/restaurant_pos/controller/UserController.java
+++ restaurant-pos/src/main/java/com/example/restaurant_pos/controller/UserController.java
@@ -2,10 +2,13 @@ package com.example.restaurant_pos.controller;
 
 
 import com.example.restaurant_pos.model.User;
+import com.example.restaurant_pos.model.request.UserRequestDTO;
 import com.example.restaurant_pos.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -21,5 +24,11 @@ public class UserController {
     @ResponseBody
     public List<User> getAllUser() {
         return userService.getAllUser();
+    }
+
+    @PostMapping("/user/register")
+    @ResponseBody
+    public User registerUser(@RequestBody UserRequestDTO userRequestDTO) {
+        return userService.registerUser(userRequestDTO);
     }
 }
--- restaurant-pos/src/main/java/com/example/restaurant_pos/service/UserService.java
+++ restaurant-pos/src/main/java/com/example/restaurant_pos/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.restaurant_pos.service;
 
 import com.example.restaurant_pos.model.User;
+import com.example.restaurant_pos.model.request.UserRequestDTO;
 import com.example.restaurant_pos.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,4 +18,12 @@ public class UserService {
         return userRepository.findAll();
 
     }
+
+    public User registerUser(UserRequestDTO userRequestDTO) {
+        User user = new User(userRequestDTO.getUsername(),
+                             userRequestDTO.getEmail(),
+                             userRequestDTO.getPassword());
+        return userRepository.save(user);
+    }
+
 }
