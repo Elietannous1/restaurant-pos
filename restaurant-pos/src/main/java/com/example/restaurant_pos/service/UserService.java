79357093360b057c3516dc7f572c908231857ac6
Elietannous1 <tannous.elie.me@gmail.com>
2025-02-17 10:28:00 +0200

    Add password hashing to user login and registration

--- restaurant-pos/src/main/java/com/example/restaurant_pos/service/UserService.java
+++ restaurant-pos/src/main/java/com/example/restaurant_pos/service/UserService.java
@@ -4,6 +4,8 @@ import com.example.restaurant_pos.model.User;
 import com.example.restaurant_pos.model.request.UserRequestDTO;
 import com.example.restaurant_pos.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -13,18 +15,20 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     public User registerUser(UserRequestDTO userRequestDTO) {
         User user = new User(userRequestDTO.getUsername(),
                 userRequestDTO.getEmail(),
-                userRequestDTO.getPassword());
+                passwordEncoder.encode(userRequestDTO.getPassword()));
         return userRepository.save(user);
     }
 
     public String loginUser(UserRequestDTO userRequestDTO) {
         User user = userRepository.findAllByEmail(userRequestDTO.getEmail());
         if (user != null) {
-            if (userRequestDTO.getEmail().equals(user.getEmail())
-                    && userRequestDTO.getPassword().equals(user.getPassword())) {
+            if (passwordEncoder.matches(userRequestDTO.getPassword(), user.getPassword())) {
                 return "Logged in successfully";
             } else {
                 return "Wrong email or password";
